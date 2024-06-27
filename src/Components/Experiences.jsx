38e06ab34503779f05e4e304c675852df0aaddf4
Aniket <aniketsahu1711@gmail.com>
2024-06-27 20:36:39 +0530

    [Refactor]:Content update

--- src/Components/Experiences.jsx
+++ src/Components/Experiences.jsx
@@ -9,7 +9,7 @@ const Experiences = () => {
             startDate: "30th May, 2024",
             endDate: "Present",
             location: "Navi Mumbai, India",
-            desc: "I am currently employed at Revergon Solutions as a Full Stack Web Developer. My role involves redesigning the company's website using Figma to significantly improve its UI and UX, aligning closely with the organization's mission and objectives. I have also developed the redesigned website in WordPress, translating my Figma designs into a functional platform. Additionally, I have had the opportunity to expand my skills by learning about AR-VR technologies and google analytics.",
+            desc: "Currently employed as an intern at Revergon Solutions as a Full Stack Web Developer. My role involves redesigning the company's website using Figma to significantly improve its UI and UX, aligning closely with the organization's mission and objectives. I have also developed the redesigned website in WordPress, translating my Figma designs into a functional platform. Additionally, I have had the opportunity to expand my skills by learning about AR-VR technologies and google analytics.",
             tags: ["Figma", "AR-VR", "Wordpress", "Google Analytics"]
         },
     ]
